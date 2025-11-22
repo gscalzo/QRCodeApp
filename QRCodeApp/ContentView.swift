e0a33efa54791a84480615041afae06ca1b2a3fa
giordano scalzo <giordano.scalzo@gmail.com>
2025-11-22 15:38:19 +0000

    implemented the empty template

---
+++ QRCodeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRCodeApp
+//
+//  Created by giordano scalzo on 22/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRCodeApp/QRCodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeAppApp.swift
+//  QRCodeApp
+//
+//  Created by giordano scalzo on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
